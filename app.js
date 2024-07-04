9384ed7b1116d0760c8ae802b668eb432517caa8
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-04 19:00:37 +0530

    implemented a basic UI that updates the moves and displays score

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let playerChoices = document.querySelector(".player-choices");
+playerChoices.addEventListener('click', (playerClicked) => {
+    let whichButton = playerClicked.target;
+    let playerSelection = document.querySelector(".player-selection");
+    let playerMove
+    switch (whichButton.id) {
+        case 'rock':
+            playerMove = 'Rock';
+            break;
+
+        case 'paper':
+            playerMove = 'Paper';
+            break;
+
+        case 'scissors':
+            playerMove = 'Scissors';
+            break;
+
+    }
+    playerSelection.textContent = playerMove;
+    run(playerMove);
+});
+
+let playerScoreDisplay = document.querySelector("#player-score");
+let computerScoreDisplay = document.querySelector("#computer-score");
+
+function updatePlayerScore()
+{
+    playerScore++;
+    playerScoreDisplay.textContent = playerScore;
+}
+
+function updateComputerScore()
+{
+    computerScore++;
+    computerScoreDisplay.textContent = computerScore;
+}
+
 
 function getComputeMove() {
     let number = Math.floor((Math.random() * 3) + 1);
+    let computerMove;
     switch (number) {
         case 1:
-            return "rock";
+            computerMove = "rock";
+            break;
         case 2:
-            return "paper";
+            computerMove = "paper";
+            break;
         case 3:
-            return "scissors";
+            computerMove = "scissors";
+            break;
     }
-
-    return NaN;
+    let computerSelction = document.querySelector(".computer-selction");
+    computerSelction.textContent = computerMove;
+    return computerMove;
 }
-
-function getPlayerMove() {
-    return prompt("Enter either rock or paper or scissor");
-
-}
-
 
 function compare(player, comp) {
     let r = "rock";
@@ -31,45 +68,25 @@ function compare(player, comp) {
         return "draw";
     else {
         if ((player === r && comp === s) || (player === s && comp === p) || (player === p && comp === r)) {
-            playerScore++;
+            
+            updatePlayerScore();
             return "player";
 
         }
         else {
-            computerScore++;
+            updateComputerScore();
             return "computer";
 
         }
     }
 }
 
-function displayScore() {
-    console.log("Player score is at : " + playerScore);
-    console.log("Computer score is at : " + computerScore);
+function run(playerMove) {
+    let comp = getComputeMove();
+    compare(playerMove, comp);
 }
 
-function run() {
-    let rounds = parseInt(prompt("Enter Number of rounds"));
-    for (let i = 1; i <= rounds; i++) {
-        console.log("Round :  " + i);
-        let player = getPlayerMove();
-        let comp = getComputeMove();
-        compare(player,comp);
-        console.log("computer move = " + comp);
-        console.log("Player move = " + player);
-        displayScore();
-        console.log("---------------------------------");
-    }
-
-    if (playerScore > computerScore)
-        console.log("Player won");
-    else if (computerScore > playerScore)
-        console.log("Computer won");
-    else
-        console.log("Draw");
-}
-
-run();
+// run();
 
 
 
